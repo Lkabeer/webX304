b5bdb161720f203d2c2c8997436c022e65c82f40
Ahmed Samir <ahmedsamir@Ahmeds-MacBook-Pro.local>
2018-11-21 02:50:05 +0200

    version X0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'chatAppX';
+
+  itemsRef: AngularFireList<any>;
+  items: Observable<any[]>;
+  user: any;
+  message: string = '';
+
+  newItem = '';
+  editMsg: boolean = false;
+  editId: number;
+
+  constructor(public db: AngularFireDatabase, public afAuth: AngularFireAuth) {
+    this.itemsRef = db.list('messages', ref => ref.limitToLast(5));
+    // Use snapshotChanges().map() to store the key
+    this.items = this.itemsRef.snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+      )
+    );
+
+    afAuth.user.subscribe(user => { 
+      if(user) {
+        this.user = user;
+        console.log(this.user);
+      }
+    });
+  }
+  
+  sendChat(newMessage: string) {
+    this.itemsRef.push({ message: newMessage, name: this.user.displayName});
+    this.message = '';
+  }
+
+  login() {
+    this.afAuth.auth.signInWithPopup(new auth.FacebookAuthProvider());
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireDatabaseModule } from "@angular/fire/database";
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReversePipe } from "../xteam/reverse.pipe";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDq4qV3Rlb1qfbFyWfVeXj0kELvaQAmaTY",
+  authDomain: "chatappx-9ce23.firebaseapp.com",
+  databaseURL: "https://chatappx-9ce23.firebaseio.com",
+  projectId: "chatappx-9ce23",
+  storageBucket: "chatappx-9ce23.appspot.com",
+  messagingSenderId: "670407836816"
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReversePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
